b1be32d5584045d70041959bcc6ece1f5b4e5843
Green Lightning <GreenLightning.git@googlemail.com>
2019-12-05 10:50:43 +0100

    Add algorithm templates.

---
+++ template_algorithm.go
@@ -0,0 +1,32 @@
+package main
+
+// Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+// Source: https://groups.google.com/d/msg/golang-nuts/PnLnr4bc9Wo/z9ZGv2DYxXoJ
+func pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+func allPermutations(values []string) (result [][]string) {
+	if len(values) == 1 {
+		result = append(result, values)
+		return
+	}
+	for i, current := range values {
+		others := make([]string, 0, len(values)-1)
+		others = append(others, values[:i]...)
+		others = append(others, values[i+1:]...)
+		for _, route := range allPermutations(others) {
+			result = append(result, append(route, current))
+		}
+	}
+	return
+}
